39f0cce395552198bcaf37d3ce0336cb00f1d59e
davu <daoanhvu122@gmail.com>
2018-11-17 12:34:36 +0700

    Adding invoice

--- src/main/java/com/bkda/simplespa/models/Invoice.java
+++ src/main/java/com/bkda/simplespa/models/Invoice.java
@@ -4,10 +4,19 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Invoice implements Serializable {
+	
+	public enum PaymentMethod {
+		CASH,
+		CREDIT_CARD,
+		DEBIT_CARD
+	}
+	
 	private long id;
 	private String number;
+	private Customer customer;
 	private Staff staff;
 	private Date startTime;
 	private Date endTime;
+	private PaymentMethod paymentMethod;
 	
 }
